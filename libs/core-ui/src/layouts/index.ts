51547c4a1f3163254733e8406d2e3c1258508dad
Hossein <fazlihossein6@gmail.com>
2025-10-26 16:28:03 +0330

    SSSS

--- libs/core-ui/src/layouts/index.ts
+++ libs/core-ui/src/layouts/index.ts
@@ -1,6 +1,8 @@
 export * from "./components";
 export * from "./blank-layout";
 export * from "./layout";
-export * from "./horizontal";
-export * from "./vertical";
 export * from "./types";
+
+// Export specific components to avoid naming conflicts
+export * from "./horizontal";
+export { LayoutAppBar as VerticalLayoutAppBar } from "./vertical";
